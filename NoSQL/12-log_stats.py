78f31a98f54bf7804f000002ddaf29ec2b641c67
Facusan2016 <facusan2016@gmail.com>
2024-01-09 19:38:42 -0300

    fix exercise 12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     """Main program"""
     client = MongoClient('mongodb://127.0.0.1:27017')
     col = client.logs.nginx
-    print(col.count_documents({}))
+    print(f'{col.count_documents({})} logs')
     methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
     print('Methods:')
     for met in methods:
