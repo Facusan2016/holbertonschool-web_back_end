9bcf50ee38f015fa5bbba8df639a1badb37d3806
Facusan2016 <facusan2016@gmail.com>
2024-01-10 14:28:38 -0300

    fix exercise 0 and added exercise 1

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -10,5 +10,6 @@ from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Function that returns the corresponding index"""
     start_page = (page - 1) * page_size
     return (start_page, start_page + page_size)
