a95e5a124d84eec4c8b0fdcce31495973cb6cedc
Facusan2016 <facusan2016@gmail.com>
2024-01-10 14:55:53 -0300

    fix exercise 2 calculations

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -60,6 +60,6 @@ class Server:
             "page": page,
             "data": paginated_dataset,
             "next_page": None if len(paginated_dataset) == 0 else page + 1,
-            "prev_page": None if page <= 0 else page - 1,
-            "total_pages": len(self.dataset()),
+            "prev_page": None if page < 2 else page - 1,
+            "total_pages": math.ceil(len(self.dataset()) / page_size),
         }
