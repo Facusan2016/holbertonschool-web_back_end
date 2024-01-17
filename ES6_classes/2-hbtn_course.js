360ff2958aaecbba1e4e8540e6961f24f8da59a1
Facusan2016 <facusan2016@gmail.com>
2024-01-17 13:07:40 -0300

    exercise 0-2 added

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,49 @@
+import { assert } from "console";
+
+export default class HolbertonCourse {
+  constructor(name, length, students){
+    
+    this.__name = name
+    this.__length = length
+    this.__students = students
+
+  }
+
+  get name() {
+    return this.__name
+  }
+
+  set name(name){
+
+    if (typeof(name) === 'string')
+    {
+      this.__name = name
+    } else {
+      throw new TypeError('Name must be a string')
+    }
+  }
+
+  get length() {
+    return this.__length
+  }
+
+  set length(l){
+    if (typeof(l) === 'number'){
+      this.__length = l
+    } else {
+      throw new TypeError('Length must be a number')
+    } 
+  }
+
+  get students() {
+    return this.__students
+  }
+
+  set students(std){
+    if (std.every(i => typeof i === 'string')){
+      this.__students == std
+    } else {
+      throw new TypeError('Students must be an array of strings')
+    }
+  }
+}
